ff53c9ad9cab3a67ea1509232136bf9374e39021
724thomas <724thomas@gmail.com>
2025-06-06 22:03:10 +0900

    refactor: problem detial. saves submission

--- src/main/java/com/example/demo/submission/domain/Submission.java
+++ src/main/java/com/example/demo/submission/domain/Submission.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 public class Submission {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String code;
