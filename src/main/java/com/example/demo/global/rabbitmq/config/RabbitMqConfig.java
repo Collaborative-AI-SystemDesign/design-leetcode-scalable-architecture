dd286f52324569c396d75d959b0ceec9386f2d8d
Gotprgmer <gofiction95@gmail.com>
2025-06-18 14:43:00 +0900

    rabbitmq 설정 수정

--- src/main/java/com/example/demo/global/rabbitmq/config/RabbitMqConfig.java
+++ src/main/java/com/example/demo/global/rabbitmq/config/RabbitMqConfig.java
@@ -6,7 +6,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -21,49 +21,70 @@ public class RabbitMqConfig {
 
     private final RabbitMqProperties rabbitMqProperties;
 
-    @Value("${rabbitmq.queue.name}")
-    private String queueName;
+    @Value("${rabbitmq.request.queue.name}")
+    private String requestQueueName;
+
+    @Value("${rabbitmq.result.queue.name}")
+    private String resultQueueName;
 
     @Value("${rabbitmq.exchange.name}")
     private String exchangeName;
 
-    @Value("${rabbitmq.routing.key}")
-    private String routingKey;
+    // 라우팅 키 분리 (application.yml 에서 정의)
+    @Value("${rabbitmq.request.routing.key}")
+    private String requestRoutingKey;
+    @Value("${rabbitmq.result.routing.key}")
+    private String resultRoutingKey;
 
     // org.springframework.amqp.core.Queue
     @Bean
-    public Queue queue() {
-        return new Queue(queueName);
+    public Queue requestQueue() {
+        return new Queue(requestQueueName);
+    }
+
+    @Bean
+    public Queue resultQueue() {
+        return new Queue(resultQueueName);
     }
 
     /**
      * 지정된 Exchange 이름으로 Direct Exchange Bean 을 생성
      */
     @Bean
-    public DirectExchange directExchange() {
-        return new DirectExchange(exchangeName);
+    public DirectExchange submissionExchange() {
+        return new DirectExchange(exchangeName, false, true);
     }
 
     /**
-     * 주어진 Queue 와 Exchange 을 Binding 하고 Routing Key 을 이용하여 Binding Bean 생성
+     * requestQueue 와 Exchange 을 Binding 하고 requestRoutingKey Key 을 이용하여 Binding Bean 생성
      * Exchange 에 Queue 을 등록한다고 이해하자
      **/
     @Bean
-    public Binding binding(Queue queue, DirectExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    public Binding requestBinding(Queue requestQueue, DirectExchange submissionExchange) {
+        return BindingBuilder.bind(requestQueue).to(submissionExchange).with(requestRoutingKey);
+    }
+
+    /**
+     * resultQueue 와 Exchange 을 Binding 하고 resultRoutingKey Key 을 이용하여 Binding Bean 생성
+     * Exchange 에 Queue 을 등록한다고 이해하자
+     **/
+    @Bean
+    public Binding resultBinding(Queue resultQueue, DirectExchange submissionExchange) {
+        return BindingBuilder.bind(resultQueue).to(submissionExchange).with(resultRoutingKey);
     }
 
     /**
      * RabbitMQ 연동을 위한 ConnectionFactory 빈을 생성하여 반환
      **/
     @Bean
-    public CachingConnectionFactory connectionFactory() {
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-        connectionFactory.setHost(rabbitMqProperties.getHost());
-        connectionFactory.setPort(rabbitMqProperties.getPort());
-        connectionFactory.setUsername(rabbitMqProperties.getUsername());
-        connectionFactory.setPassword(rabbitMqProperties.getPassword());
-        return connectionFactory;
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory cf,
+            MessageConverter converter) {
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(cf);
+        factory.setMessageConverter(converter);
+        return factory;
     }
 
     /**
