130d4b7cab9461625969dcc1e2e8af4827b6d09d
Gotprgmer <gofiction95@gmail.com>
2025-06-18 15:33:33 +0900

    consume of spring

--- src/main/java/com/example/demo/global/rabbitmq/RabbitMqService.java
+++ src/main/java/com/example/demo/global/rabbitmq/RabbitMqService.java
@@ -1,17 +1,14 @@
 package com.example.demo.global.rabbitmq;
 
 
-import com.example.demo.global.common.ProblemDto;
-import com.example.demo.global.common.SubmissionMessageDto;
 import com.example.demo.global.common.TestcaseDto;
-import com.example.demo.global.common.UserDto;
 import com.example.demo.global.enums.SubmissionStatus;
-import com.example.demo.problem.controller.request.SubmissionRequest;
-import com.example.demo.problem.domain.Problem;
+import com.example.demo.leaderboard.application.LeaderBoardService;
+import com.example.demo.problem.controller.request.SubmissionRequestMessageDto;
+import com.example.demo.problem.controller.response.SubmissionResultMessageDto;
 import com.example.demo.submission.domain.Submission;
 import com.example.demo.submission.domain.api.SubmissionRepository;
-import com.example.demo.user.domain.User;
-import com.example.demo.user.domain.api.UserApiRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -20,9 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Queue 로 메세지를 발행할 때에는 RabbitTemplate 의 ConvertAndSend 메소드를 사용하고
@@ -34,18 +29,17 @@ import java.util.Random;
 @Service
 public class RabbitMqService {
 
-    @Value("${rabbitmq.queue.name}")
-    private String queueName;
 
     @Value("${rabbitmq.exchange.name}")
     private String exchangeName;
 
-    @Value("${rabbitmq.routing.key}")
-    private String routingKey;
+    @Value("${rabbitmq.request.routing.key}")
+    private String requestRoutingKey;
 
     private final RabbitTemplate rabbitTemplate;
+
     private final SubmissionRepository submissionRepository;
-    private final UserApiRepository userApiRepository;
+    private final LeaderBoardService leaderboardService;
 
     /**
      * 1. Queue 로 메세지를 발행
@@ -54,59 +48,30 @@ public class RabbitMqService {
      *
      * @return
      */
-    public void sendMessage(SubmissionMessageDto messageDto) {
-        log.info("************ messagge send: {}",messageDto.getProblemDto());
-        this.rabbitTemplate.convertAndSend(exchangeName,routingKey,messageDto);
+    public void sendMessage(SubmissionRequestMessageDto submissionRequestMessageDto) {
+        log.info("************ messagge send: {}", submissionRequestMessageDto.getSubmissionId());
+        this.rabbitTemplate.convertAndSend(exchangeName,requestRoutingKey,submissionRequestMessageDto);
     }
 
     /**
      * 1. Queue 에서 메세지를 받도록 함.
-     * 2. 임의로 messageDto를 Object 타입으로 받았지만, 실제로는 DTO 클래스를 사용하여 타입을 지정.
      **/
-//    @RabbitListener(queues = "${rabbitmq.queue.name}")
-//    @Transactional
-//    public void receiveMessage(SubmissionMessageDto messageDto) {
-//        log.info("************ messagge receive: {}",messageDto.getProblemDto());
-//        ProblemDto problemDto = messageDto.getProblemDto();
-//        UserDto userDto = messageDto.getUserDto();
-//        SubmissionRequest request = messageDto.getRequest();
-//
-//        List<TestcaseDto> testcases = problemDto.getTestcases();
-//
-//        String executableCode = generateExecutableCode(request.getCode(), testcases);
-//        /* Todo: 실제로 샌드박스 환경에서 실행하는 코드로 변경하고 요청을 보내야합니다.
-//        String stdout = sandboxApi.execute(executableCode);
-//        List<Boolean> testResults = Arrays.stream(
-//                        stdout.replaceAll("[\\[\\] ]", "").split(","))
-//                .map(Boolean::parseBoolean)
-//                .toList();
-//        */
-//
-//        List<SubmissionStatus> testResults = new ArrayList<>();
-//        for (int i=0; i < testcases.size(); i++) {
-//            testResults.add(SubmissionStatus.SUCCESS);
-//        }
-//
-//        double runtime = new Random().nextDouble(0.1, 2.0); // Simulate runtime in seconds
-//        double memory = new Random().nextDouble(10, 100); // Simulate memory usage in MB
-//        sleep((int)runtime*1000); // Simulate execution time
-//
-//        User user = User.toEntity(userDto.getId(), userDto.getNickname());
-//        Problem problem = Problem.toEntity(problemDto);
-//
-//        // 결과 받아서 저장하기
-//        Submission submission = Submission.toEntity(
-//                request.getCode(),
-//                request.getCodingLanguage(),
-//                SubmissionStatus.SUCCESS, // 실제로는 testResults에 따라 다르게 설정해야 합니다.
-//                runtime,
-//                memory,
-//                user,
-//                problem
-//        );
-//
-//        submissionRepository.save(submission);
-//    }
+    @Transactional
+    @RabbitListener(queues = "${rabbitmq.result.queue.name}")
+    public void handleResult(SubmissionResultMessageDto submissionResultMessageDto) {
+        // 1) 기존 Submission 엔티티 조회
+        Long submissionId = submissionResultMessageDto.getSubmissionId();
+        SubmissionStatus status = submissionResultMessageDto.getStatus();
+        Submission submission = submissionRepository.findById(submissionId)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Submission not found: " + submissionId));
+
+        // 2) 상태 업데이트
+        submission.setStatus(status);// SUCCESS or FAILURE
+
+        submissionRepository.save(submission);
+        leaderboardService.saveLeaderboard( submissionResultMessageDto.getContestId(), submission.getUser());
+    }
 
     /**
      * 샌드박스 환경에서 실행할 수 있는 Java 프로그램 코드를 생성합니다.
--- src/main/java/com/example/demo/leaderboard/application/LeaderBoardService.java
+++ src/main/java/com/example/demo/leaderboard/application/LeaderBoardService.java
@@ -1,20 +1,26 @@
 package com.example.demo.leaderboard.application;
 
 
+import com.example.demo.contest.domain.Contest;
+import com.example.demo.contest.domain.api.ContestRepository;
 import com.example.demo.leaderboard.controller.response.LeaderBoardResponse;
+import com.example.demo.leaderboard.domain.LeaderBoard;
 import com.example.demo.leaderboard.domain.RankingEntry;
 import com.example.demo.leaderboard.domain.api.LeaderBoardRepository;
+import com.example.demo.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
 public class LeaderBoardService {
     private final LeaderBoardRepository leaderBoardRepository;
     private final LeaderBoardRedisService leaderBoardRedisService;
+    private final ContestRepository contestRepository;
 
     public LeaderBoardResponse getLeaderBoardInfo(Long contestId) {
 
@@ -51,5 +57,27 @@ public class LeaderBoardService {
 //        }
 
         return LeaderBoardResponse.from(rankingEntries);
+    }
+
+    @Transactional
+    public void saveLeaderboard(Long contestId, User user) {
+
+        if (contestId != null) {
+            Contest contest = contestRepository.findById(contestId).orElseThrow();
+//            // 테스트용 코드로 score를 500 ~ 999점 사이로 넣는다.
+//            int score = 500 + new Random().nextInt(999 - 500 + 1);
+//            // 100~ 4000 사이로 랜덤값이 나오는데 score에 따라서 계산됨.
+//            long timeTaken = (long) (4000 - (4000 - 100) * (new Random().nextInt(1000) / 999.0));
+
+            int score = ThreadLocalRandom.current().nextInt(500, 999);
+            long timeTaken = ThreadLocalRandom.current().nextLong(100, 4000);
+
+            // save user score info to Redis
+            leaderBoardRedisService.addScore(contestId,user.getId(), score);
+
+            // save leaderboard info to the RDB
+            LeaderBoard leaderBoard = LeaderBoard.toEntity(score, timeTaken, contest, user);
+            leaderBoardRepository.save(leaderBoard);
+        }
     }
 }
